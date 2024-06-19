342e22a355374111751a84310174cbbc8a75a99e
jhencams <zhenboylescamance@gmail.com>
2024-06-18 21:16:42 -0700

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,19 @@
+word = "summer bootcamp"
+
+print(word.title())
+print(word.capitalize().replace("p","P"))
+print(word[7:11].replace("b","L"))
+print(word[11:15].replace("p","E"))
+print(word[12].upper() + word[5].upper())
+print(word.replace(" ","ss").isalpha())
+
+
+
+
+
+
+
+
+
+
+
