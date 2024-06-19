b89ddc50d7b1eab4815c94ea68408579fdb9657c
jhencams <zhenboylescamance@gmail.com>
2024-06-19 00:05:32 -0700

    Update act1.py

--- act1.py
+++ act1.py
@@ -4,7 +4,7 @@ print(word.title())
 print(word.capitalize().replace("p","P"))
 print(word[7:11].replace("b","L"))
 print(word[11:15].replace("p","E"))
-print(word[12].upper() + word[5].upper())
+print(word[-3].upper() + word[5].upper())
 print(word.replace(" ","ss").isalpha())
 
 
